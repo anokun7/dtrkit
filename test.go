f5e0b38ef49741be81785636f1e3bcd234a8f573
Anoop V Kumar <anoop@docker.com>
2017-09-02 17:14:40 -0400

    Test connectivity to DTR

---
+++ test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "net/http"
+  "io/ioutil"
+)
+
+func main() {
+  client := &http.Client{}
+
+  req, err := http.NewRequest("GET","https://dtr.noop.ga/api/v0/repositories", nil)
+  req.SetBasicAuth("admin", "OrcaOrca")
+  resp, err := client.Do(req)
+  if err != nil {
+  }
+  json, err := ioutil.ReadAll(resp.Body)
+  resp.Body.Close()
+  println(string(json))
+}
